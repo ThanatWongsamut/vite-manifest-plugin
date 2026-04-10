56f369f9fad3970b8243a4d8b554de0df32b6d1a
Thanat Wongsamut <thanat.wongsamut@gmail.com>
2026-04-11 01:22:15 +0700

    Re-export types from package entry point

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Plugin } from "vite"
 import { ManifestOptions } from "./types"
 import { modifiedManifest } from "./utils"
 
+export type { ManifestOptions, ManifestEntry, ManifestValue } from "./types"
+
 export const viteManifestPlugin = async (options: ManifestOptions): Promise<Plugin> => {
   const plugin: Plugin = {
     name: 'vite-manifest-plugin',
